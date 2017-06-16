21d494835de0a8b4da4050b206cd7daa3757a06d
Triet Cao <triet@bitcoin.com>
2017-06-16 02:00:25 -0700

    Return true for IsValidHexAddress.

--- util/util.go
+++ util/util.go
@@ -18,9 +18,10 @@ var addressPattern = regexp.MustCompile("^0x[0-9a-fA-F]{40}$")
 var zeroHash = regexp.MustCompile("^0?x?0+$")
 
 func IsValidHexAddress(s string) bool {
-	if IsZeroHash(s) || !addressPattern.MatchString(s) {
+	// BCom modification: Allow any string to be a username, instead of just hex addresses
+	/*if IsZeroHash(s) || !addressPattern.MatchString(s) {
 		return false
-	}
+	}*/
 	return true
 }
 
