9cf879a2f03d76956bbd7f4b8a6dd1801e91f315
Triet Cao <triet@bitcoin.com>
2017-06-16 00:52:43 -0700

    Change Host URLs to our server.

--- www/config/environment.js
+++ www/config/environment.js
@@ -15,14 +15,14 @@ module.exports = function(environment) {
 
     APP: {
       // API host and port
-      ApiUrl: '//example.net/',
+      ApiUrl: '//199.48.147.21/',
 
       // HTTP mining endpoint
-      HttpHost: 'http://example.net',
+      HttpHost: 'http://199.48.147.21',
       HttpPort: 8888,
 
       // Stratum mining endpoint
-      StratumHost: 'example.net',
+      StratumHost: '199.48.147.21',
       StratumPort: 8008,
 
       // Fee and payout details
